57a8c37766da725b0bc4d4621d565d8052b0af6a
Frederic Prochnow <frederic.prochnow@numericable.fr>
2016-04-22 14:49:30 +0200

    22042016 1449

--- src/main/java/iut/tdd/RomanNumerals.java
+++ src/main/java/iut/tdd/RomanNumerals.java
@@ -3,15 +3,25 @@ package iut.tdd;
 import java.util.ArrayList;
 
 public class RomanNumerals {
+	String[] chiffrearabe = {"1","2","3","4","5","6","7","8","9","10"};
+	String[] chiffreromain = {"I","II","III","IV","V","VI","VII","VIII","IX","X"};
+	
 	public String convertToRoman(String arabe) {
-		String[] chiffrearabe = {"1","2","3","4","5","6","7","8","9","10"};
-		String[] chiffreroman = {"I","II","III","IV","V","VI","VII","VIII","IX","X"};
-		for(int i= 0)
+		for(int i= 0; i < chiffrearabe.length ; i++) {
+			if(chiffrearabe[i].equals(arabe)){
+				return chiffreromain[i];
+			}
+		}
 		return null;
 	}
 
-	public String convertFromRoman(String roman) {
-		return "1";
+	public String convertFromRoman(String romain) {
+		for(int i= 0; i < chiffrearabe.length ; i++) {
+			if(chiffreromain[i].equals(romain)){
+				return chiffrearabe[i];
+			}
+		}
+		return null;
 	}
 
 }
